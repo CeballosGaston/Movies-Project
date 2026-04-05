e551f4366ad89ec83a48d1c2fecee475c04dc080
Gastón Ceballos <ceballoscallerosgaston@gmail.com>
2026-04-05 14:58:52 +0200

    feat: favoriteContext added

---
+++ src/context/FavoritesContext.tsx
@@ -0,0 +1,33 @@
+import { createContext, useContext, useState } from "react";
+import type { Movie } from "../features/movies/movies.types";
+
+interface FavoritesContextType {
+  favorites: Movie[];
+  addFavorite: (movie: Movie) => void;
+  removeFavorite: (id: number) => void;
+  isFavorite: (id: number) => boolean;
+}
+
+const FavoritesContext = createContext<FavoritesContextType | undefined>(undefined);
+
+export const FavoritesProvider = ({ children }: { children: React.ReactNode }) => {
+  const [favorites, setFavorites] = useState<Movie[]>([]);
+
+  const addFavorite = (movie: Movie) => {
+    setFavorites((prev) => [...prev, movie]);
+  };
+
+  const removeFavorite = (id: number) => {
+    setFavorites((prev) => prev.filter((m) => m.id !== id));
+  };
+
+  const isFavorite = (id: number) => {
+    return favorites.some((m) => m.id === id);
+  };
+
+  return (
+    <FavoritesContext.Provider value={{ favorites, addFavorite, removeFavorite, isFavorite }}>
+      {children}
+    </FavoritesContext.Provider>
+  );
+};
